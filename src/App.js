cb59d4410991b6ba92e7975c947565b14e774954
Victor <victorlima0302@gmail.com>
2024-10-02 18:19:05 -0300

    feat: add tela login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import TelaInicioAdmin from './pages/Admin/TelaInicio';
 import GlobalStyle from './GlobalStyle';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+
+import TelaInicioAdmin from './pages/Admin/TelaInicio';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
       <GlobalStyle />
       <Routes>
         <Route path="/" element={<TelaInicioAdmin />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/Login.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import styled from 'styled-components';
+import logo from '../assets/Logo 1.png';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background: rgb(84,41,125);
+  background: linear-gradient(127deg, rgba(84,41,125,1) 0%, rgba(19,33,91,1) 47%, rgba(1,105,126,1) 100%);
+  
+  @media (max-width: 768px) {
+    padding: 20px;
+  }
+`;
+
+const Logo = styled.img`
+  width: 350px;
+  margin-bottom: 80px;
+
+  @media (max-width: 768px) {
+    width: 320px;
+  }
+`;
+
+const LoginBox = styled.div`
+  background-color: rgba(0, 0, 0, 0.6);
+  padding: 60px 30px;
+  border-radius: 12px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
+  text-align: center;
+  width: 100%;
+  max-width: 400px;
+  min-height: 400px;
+
+  @media (max-width: 768px) {
+    padding: 40px 20px;
+    min-height: 300px;
+  }
+`;
+
+const Title = styled.h2`
+  color: white;
+  margin-bottom: 40px;
+  font-size: 32px;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  padding: 12px;
+  margin-bottom: 35px;
+  border: none;
+  border-radius: 8px;
+  background-color: #f3f3f3;
+`;
+
+const Button = styled.button`
+  width: 100%;
+  padding: 12px;
+  background-color: #009fe3;
+  border: none;
+  border-radius: 8px;
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+  margin-top: 35px;
+  margin-bottom: 30px;
+  font-size: 18px;
+
+  &:hover {
+    background-color: #007bbd;
+  }
+`;
+
+const ForgotPassword = styled.a`
+  color: white;
+  font-size: 0.9rem;
+  text-decoration: none;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const Login = () => {
+    return (
+        <Container>
+            <Logo src={logo} alt="Logo Faminas" />
+            <LoginBox>
+                <Title>Agendamento de Quadras</Title>
+                <Input type="text" placeholder="E-mail" />
+                <Input type="password" placeholder="Senha" />
+                <Button>Acessar</Button>
+                <ForgotPassword href="#">Esqueci minha senha</ForgotPassword>
+            </LoginBox>
+        </Container>
+    );
+};
+
+export default Login;
